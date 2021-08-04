b252dd4c31e95d5d7a7002ec95327bc79530018e
nancy.kigotho@student.moringaschool.com <nancykigotho14@gmail.com>
2021-08-04 20:38:38 +0300

    indent my work

--- contacts.js
+++ contacts.js
@@ -5,35 +5,14 @@ btn.addEventListener("click", function () {
     var email = document.getElementById("email").value;
     var subject = document.getElementById("subject").value;
     var message = document.getElementById("message").value;
+
     //control structure//
     if (name == "" || email == "" || subject=="" || message == "") {
-        $("#pop-message").append(person + ", We are processing your order and it will be delivered to you at " + location + ". Prepare sh. " + deliveryAmount + ". It is inclusive the delivery fee and your order bill");
-        $("#pop-message").slideDown(1200);
-        $("#pop-message").delay(3000).slideUp(1200);
-    } else {
-        $.ajax({
-            type: "POST",
-            url: "http://localhost:8080/api/contacts",
-            data: {
-                name: name,
-                email: email,
-                subject: subject,
-                message: message
-            },
-            success: function (data) {
-                $("#pop-message").append(person + ", We are processing your order and it will be delivered to you at " + location + ". Prepare sh. " + deliveryAmount + ". It is inclusive the delivery fee and your order bill");
-                $("#pop-message").slideDown(1200);
-                $("#pop-message").delay(3000).slideUp(1200);
-            }
-        });
-    }
-});
-});
 
-        // alert("Please fill all the fields");
-//     }
-//     else
-//         alert(name + "" + "" + ",we have received your message,Thank you for reaching out to us😎")
-// });
-// });
+        alert("Please fill all the fields");
+    }
+    else
+        alert(name + "" + "" + ",we have received your message,Thank you for reaching out to us😎")
+});
+});
 
