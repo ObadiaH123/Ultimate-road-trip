d48967adcfa9ca3e5db2ac7e59d45326956fb7a6
nancy.kigotho@student.moringaschool.com <nancykigotho14@gmail.com>
2021-08-04 17:00:12 +0300

    modify my js file

--- contacts.js
+++ contacts.js
@@ -7,10 +7,33 @@ btn.addEventListener("click", function () {
     var message = document.getElementById("message").value;
     //control structure//
     if (name == "" || email == "" || subject=="" || message == "") {
-        alert("Please fill all the fields");
+        $("#pop-message").append(person + ", We are processing your order and it will be delivered to you at " + location + ". Prepare sh. " + deliveryAmount + ". It is inclusive the delivery fee and your order bill");
+        $("#pop-message").slideDown(1200);
+        $("#pop-message").delay(3000).slideUp(1200);
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8080/api/contacts",
+            data: {
+                name: name,
+                email: email,
+                subject: subject,
+                message: message
+            },
+            success: function (data) {
+                $("#pop-message").append(person + ", We are processing your order and it will be delivered to you at " + location + ". Prepare sh. " + deliveryAmount + ". It is inclusive the delivery fee and your order bill");
+                $("#pop-message").slideDown(1200);
+                $("#pop-message").delay(3000).slideUp(1200);
+            }
+        });
     }
-    else
-        alert(name + "" + "" + ",we have received your message,Thank you for reaching out to us😎")
 });
 });
+
+        // alert("Please fill all the fields");
+//     }
+//     else
+//         alert(name + "" + "" + ",we have received your message,Thank you for reaching out to us😎")
+// });
+// });
 
