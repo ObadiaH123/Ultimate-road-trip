62e7e1d576dc28fcce937c84b94cb2bd9e957929
viola-kaveza <viola.kaveza@student.moringaschool.com>
2021-08-06 07:51:45 +0300

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
-document.forms['forms'].addEventListener('submit', function (e) {
-    e.preventDefault();
-});
-function checkEmpty() {
-    var name = document.forms['forms'].elements[0].value;
-    var email = document.forms['forms'].elements[1].value;
-    var message = document.forms['forms'].elements[2].value;
+// document.forms['forms'].addEventListener('submit', function (e) {
+//     e.preventDefault();
+// });
+// function checkEmpty() {
+//     var name = document.forms['forms'].elements[0].value;
+//     var email = document.forms['forms'].elements[1].value;
+//     var message = document.forms['forms'].elements[2].value;
 
-    if (name == "" && email == "") {
-        alert('please enter your details');
-        return 0;
-    }
-    else {
-        if (name == "") {
-            alert("please enter your name!");
-        }
-        else if (email == "") {
-            alert("please enter your email!");
-        }
-    }
-    if ((name !== "" && email != "" && text == "") || (name != "" && email != "" && text != "")) {
-        alert("Dear " + name + " your message was received successfully.");
-        document.getElementById('results').innerHTML = "Dear " + name + " your message was received successfully you will be contacted soon. Thank you.";
-    }
-}
+//     if (name == "" && email == "") {
+//         alert('please enter your details');
+//         return 0;
+//     }
+//     else {
+//         if (name == "") {
+//             alert("please enter your name!");
+//         }
+//         else if (email == "") {
+//             alert("please enter your email!");
+//         }
+//     }
+//     if ((name !== "" && email != "" && text == "") || (name != "" && email != "" && text != "")) {
+//         alert("Dear " + name + " your message was received successfully.");
+//         document.getElementById('results').innerHTML = "Dear " + name + " your message was received successfully you will be contacted soon. Thank you.";
+//     }
+// }
     
 // (document).ready(function () {
 //     var btn = document.getElementById("send-message");
@@ -37,10 +37,10 @@ function checkEmpty() {
 //             alert("Please fill all the fields");()
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    $("#booknow").click(function(addEventListener){
-        var names =$("#appointment-form").val();
-        alert(names)
-    })
-})
+//     $("#booknow").click(function(addEventListener){
+//         var names =$("#appointment-form").val();
+//         alert(names)
+//     })
+// })
